6ce394dd93eb2a9b63a52203fecbe25e940faf04
riadh <tmrfcb@gmail.com>
2016-12-09 21:20:13 +0100

    Add files via upload

---
+++ web/src/dashboard/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'my-dashboard',
+  templateUrl: 'dashboard.component.html',
+  styleUrls: ['dashboard.component.css']
+})
+export class DashboardComponent{
+
+}
